f4c8b36c55415d483767a9ff169b70721f87f134
Jesus T <flareon831@gmail.com>
2020-07-11 13:03:46 -0700

    promotion schema
    not sure if right

--- models/promotion.js
+++ models/promotion.js
@@ -5,5 +5,38 @@ require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
 const promotionSchema = new Schema({
-    name
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }, 
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
 })
+
+const Promotion = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotion;
